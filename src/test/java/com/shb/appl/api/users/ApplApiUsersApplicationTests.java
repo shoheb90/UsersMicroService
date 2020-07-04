a4438997e72c3891d6e3afea68afa4e57fc24196
shbtechno <shbtechno@gmail.com>
2020-07-04 16:03:40 +0100

    First project commit

---
+++ src/test/java/com/shb/appl/api/users/ApplApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shb.appl.api.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
