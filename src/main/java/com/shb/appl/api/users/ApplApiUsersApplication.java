e341701e79d9dc962c7f4daa19067d27db936198
shbtechno <shbtechno@gmail.com>
2020-07-04 18:40:11 +0100

    Enable Spring discovery Cloud Client - @EnableEurekaClient

--- src/main/java/com/shb/appl/api/users/ApplApiUsersApplication.java
+++ src/main/java/com/shb/appl/api/users/ApplApiUsersApplication.java
@@ -2,7 +2,11 @@ package com.shb.appl.api.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+/*
+	@EnableEurekaClient - to consider this service as a client to eureka server
+ */
+@EnableEurekaClient
 @SpringBootApplication
 public class ApplApiUsersApplication {
 
