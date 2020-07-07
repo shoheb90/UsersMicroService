8a89391aae18b1585a907b2faabc7aa4e2b5ec74
shbtechno <shbtechno@gmail.com>
2020-07-07 14:26:51 +0100

    Created Service and User DTO

---
+++ src/main/java/com/shb/appl/api/users/ui/service/UserService.java
@@ -0,0 +1,15 @@
+package com.shb.appl.api.users.ui.service;
+/*
+   @Author      : Shoheb
+   Description  : 
+   >>>>>>>>>>>>>>>>>>>>> Annotations >>>>>>>>>>>>>>>>>>>>>  
+   @      :   
+   @      :   
+   
+ */
+
+import com.shb.appl.api.users.ui.shared.UserDto;
+
+public interface UserService {
+   UserDto createUser(UserDto userDetails);
+}
---
+++ src/main/java/com/shb/appl/api/users/ui/service/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.shb.appl.api.users.ui.service;
+/*
+   @Author      : Shoheb
+   Description  : 
+   >>>>>>>>>>>>>>>>>>>>> Annotations >>>>>>>>>>>>>>>>>>>>>  
+   @      :   
+   @      :   
+   
+ */
+
+import com.shb.appl.api.users.ui.shared.UserDto;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserDto createUser(UserDto userDetails) {
+        return null;
+    }
+}
