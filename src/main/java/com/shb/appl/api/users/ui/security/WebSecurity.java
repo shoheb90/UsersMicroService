7066988c0c9532ca498590f2321eaacd52e7b8fe
shbtechno <shbtechno@gmail.com>
2020-07-09 20:42:24 +0100

    Added WebSecurity configuration to permit users services

--- src/main/java/com/shb/appl/api/users/ui/security/WebSecurity.java
+++ src/main/java/com/shb/appl/api/users/ui/security/WebSecurity.java
@@ -20,7 +20,18 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
+        /*
+            CSRF stands for Cross-Site Request Forgery.
+            It is an attack that forces an
+            end user to execute unwanted actions on a web application in which they are currently authenticated
+            Eg : user is asked to change email address or change password
+
+         */
         http.csrf().disable();
+
+        /*
+            Temporary permitted users service requests
+         */
         http.authorizeRequests().antMatchers("/users/**").permitAll();
     }
 }
